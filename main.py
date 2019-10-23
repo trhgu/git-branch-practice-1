ae0d0019d7bc33a1a0e8770a35cb1600a4634f4e
thrgu <janyty@snu.ac.kr>
2019-10-23 16:46:55 +0900

    feature : change function
    I change funtion 15 to 3*5

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 for i in range(1,100+1):
-    if i % 15==0:
+    if i % (3*5)==0:
         print("fizzbuzz")
-    elif i % 3==0:
+    elif i % (1*3)==0:
         print("fizz")
     elif i % 5==0:
         print("buzz")
